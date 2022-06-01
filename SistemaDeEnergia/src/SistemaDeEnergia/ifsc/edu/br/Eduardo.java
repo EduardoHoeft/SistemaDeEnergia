e19c765af3de7c23c7c1f1327175591eae54dfbb
aluno <eduardofelipehoeft@gmail.com>
2022-06-01 15:57:34 -0300

    commit do eduardo

--- SistemaDeEnergia/src/SistemaDeEnergia/ifsc/edu/br/Eduardo.java
+++ SistemaDeEnergia/src/SistemaDeEnergia/ifsc/edu/br/Eduardo.java
@@ -3,6 +3,7 @@ package SistemaDeEnergia.ifsc.edu.br;
 public class Eduardo {
 	
 public void stringImprimir() {
-		System.out.println("I am Eduardokkk!");
+		System.out.println("I am Eduardo!");
+		System.out.println("I am Eduardo!");
 	}
 }
